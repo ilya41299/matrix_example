58891fe1f85ba4594c4e890baa33c69af36c8bbf
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2018-03-21 19:03:03 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,27 +1,29 @@
 #include <iostream>
 
+template <typename T>
+
 class matrix_t {
 private:
-	float ** elements_;
+	T ** elements_;
 	std::size_t rows_;
 	std::size_t collumns_;
 public:
-	matrix_t();
-	matrix_t( matrix_t const & other );
-	matrix_t & operator =( matrix_t const & other );
-	~matrix_t();
-    
-    std::size_t rows() const;
-    std::size_t collumns() const;
+	matrix_t<T>();
+	matrix_t<T>(matrix_t<T>const & other);
+	matrix_t<T> & operator =(matrix_t<T> const & other);
+	~matrix_t<T>();
 
-	matrix_t operator +( matrix_t const & other ) const;
-	matrix_t operator -( matrix_t const & other ) const;
-	matrix_t operator *( matrix_t const & other ) const;
+	std::size_t rows() const;
+	std::size_t collumns() const;
 
-	matrix_t & operator -=( matrix_t const & other );
-	matrix_t & operator +=( matrix_t const & other );
-	matrix_t & operator *=( matrix_t const & other );
+	matrix_t<T> operator +(matrix_t<T> const & other) const;
+	matrix_t<T> operator -(matrix_t<T> const & other) const;
+	matrix_t operator *(matrix_t const & other) const;
 
-	std::istream & read( std::istream & stream );
-	std::ostream & write( std::ostream  & stream ) const;
+	matrix_t<T> & operator -=(matrix_t<T> const & other);
+	matrix_t<T> & operator +=(matrix_t<T> const & other);
+	matrix_t<T> & operator *=(matrix_t<T> const & other);
+
+	std::istream & read(std::istream & stream);
+	std::ostream & write(std::ostream  & stream) const;
 };
