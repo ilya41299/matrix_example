e2ab72e3f26b54a7e19bb1ea16287aee96fdf907
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2018-03-21 13:52:18 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -8,22 +8,22 @@ private:
 	std::size_t collumns_;
 public:
 	matrix_t();
-	matrix_t( matrix_t const & other );
-	matrix_t & operator =( matrix_t const & other );
+	matrix_t( matrix_t<T> const & other );
+	matrix_t<T> & operator =( matrix_t<T> const & other );
 	~matrix_t();
     
     std::size_t rows() const;
     std::size_t collumns() const;
 
-	matrix_t operator +( matrix_t const & other ) const;
-	matrix_t operator -( matrix_t const & other ) const;
-	matrix_t operator *( matrix_t const & other ) const;
+	matrix_t<T> operator +( matrix_t<T> const & other ) const;
+	matrix_t<T> operator -( matrix_t<T> const & other ) const;
+	matrix_t<T> operator *( matrix_t<T> const & other ) const;
 
-	matrix_t & operator -=( matrix_t const & other );
-	matrix_t & operator +=( matrix_t const & other );
-	matrix_t & operator *=( matrix_t const & other );
+	matrix_t<T> & operator -=( matrix_t<T> const & other );
+	matrix_t<T> & operator +=( matrix_t<T> const & other );
+	matrix_t<T> & operator *=( matrix_t<T> const & other );
 	
-	bool success (matrix_t const & one, char op);
+	bool success (matrix_t<T> const & one, char op);
 
 	std::istream & read( std::istream & stream );
 	std::ostream & write( std::ostream  & stream ) const;
