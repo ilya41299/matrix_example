d50433a41d6b0ace6c287a2fa3e04ee20436bdec
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2018-03-21 19:50:15 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,197 +1,240 @@
 #include "matrix.hpp"
 
 template <typename T>
-matrix_t<T>::matrix_t() : elements_{ nullptr }, rows_{ 0 }, collumns_{ 0 }
+matrix_t<T>::matrix_t()
+	: elements_{ nullptr }, rows_{ 0 }, collumns_{ 0 }
 {
 }
 
 template <typename T>
-matrix_t<T>::matrix_t( matrix_t<T> const & other )
+matrix_t<T>::matrix_t(matrix_t<T> const& other)
 {
 	rows_ = other.rows_;
 	collumns_ = other.collumns_;
-	elements_ = new T *[ rows_];
-	for (std::size_t i = 0; i <  rows_; ++i) {
-	elements_[i] = new T [collumns_];
-		for (std::size_t j = 0; j < collumns_; ++j) {
+	elements_ = new T*[rows_];
+	for (std::size_t i = 0; i < rows_; ++i)
+	{
+		elements_[i] = new T [this->collumns_];
+		for (std::size_t j = 0; j < collumns_; ++j)
+		{
 			elements_[i][j] = other.elements_[i][j];
 		}
 	}
 }
 
 template <typename T>
-matrix_t<T> & matrix_t<T>::operator =( matrix_t<T> const & other )
+matrix_t<T>& matrix_t<T>::operator=(matrix_t<T>const& other)
 {
-	for (std::size_t i = 0; i < rows_; ++i) {
-		delete[] elements_[i];
-	}
-	delete[] elements_;
-	rows_ = other.rows_;
-	collumns_ = other.collumns_;
-	elements_ = new T * [rows_];
-	for (std::size_t i = 0; i < rows_; ++i) {
-		elements_[i] = new T [collumns_];
-		for (std::size_t j = 0; j < collumns_; ++j) {
-			elements_[i][j] = other.elements_[i][j];
+	if (this != &other)
+	{
+		if (elements_ != nullptr && rows_ && collumns_)
+		{
+			for (std::size_t i = 0; i < rows_; ++i)
+			{
+				delete[] elements_[i];
+			}
+			delete[] elements_;
 		}
-	}			
+		rows_ = other.rows_;
+		collumns_ = other.collumns_;
+		elements_ = new T*[rows_];
+		for (std::size_t i = 0; i < rows_; ++i)
+		{
+			elements_[i] = new T[collumns_];
+			for (std::size_t j = 0; j < collumns_; ++j)
+			{
+				this->elements_[i][j] = other.elements_[i][j];
+			}
+		}
+	}
 	return *this;
 }
 
 template <typename T>
 matrix_t<T>::~matrix_t()
 {
-	for (std::size_t i = 0; i < rows_; i++) {
-		delete [] elements_[i];
+	for (std::size_t i = 0; i < this->rows_; i++)
+	{
+		delete[] this->elements_[i];
 	}
-	delete []elements_;
+	delete[] this->elements_;
+	collumns_ = 0;
+	rows_ = 0;
 }
 
 template <typename T>
 std::size_t matrix_t<T>::rows() const
 {
-    return rows_;
+	return rows_;
 }
 
 template <typename T>
 std::size_t matrix_t<T>::collumns() const
 {
-    return collumns_;
+	return collumns_;
 }
 
 template <typename T>
-matrix_t<T> matrix_t<T>::operator +( matrix_t<T> const & other ) const
+matrix_t matrix_t<T>::operator+(matrix_t<T> const& other) const
 {
 	matrix_t<T> result;
-	
-	if (rows_ == other.rows_ && collumns_ == other.collumns_) {
-	  	result.elements_ = new T *[rows_];
-		for (std::size_t i = 0; i<rows_; i++) {
-			result.elements_[i] = new T [collumns_];
+
+	if (rows_ == other.rows_ && collumns_ == other.collumns_)
+	{
+		result.elements_ = new T*[rows_];
+		for (std::size_t i = 0; i < rows_; i++)
+		{
+			result.elements_[i] = new T[collumns_];
 		}
 		result.rows_ = rows_;
 		result.collumns_ = collumns_;
-		
-		for (std::size_t i = 0; i<rows_; i++) {
-			for (std::size_t j = 0; j<collumns_; j++) {
+
+		for (std::size_t i = 0; i < rows_; i++)
+		{
+			for (std::size_t j = 0; j < collumns_; j++)
+			{
 				result.elements_[i][j] = elements_[i][j] + other.elements_[i][j];
 			}
 		}
 	}
-	else {
-		throw false;
+	else
+	{
+		std::cout << "An error has occured while reading input data\n";
 	}
 
 	return result;
 }
 
 template <typename T>
-matrix_t<T> matrix_t<T>::operator -( matrix_t<T> const & other ) const
+matrix_t<T> matrix_t<T>::operator-(matrix_t<T> const& other) const
 {
 	matrix_t<T> result;
-	
-	if (rows_ == other.rows_ && collumns_ == other.collumns_) {
-	  	result.elements_ = new T *[rows_];
-		for (std::size_t i = 0; i<rows_; i++) {
-			result.elements_[i] = new T [collumns_];
+
+	if (rows_ == other.rows_ && collumns_ == other.collumns_)
+	{
+		result.elements_ = new T*[rows_];
+		for (std::size_t i = 0; i < rows_; i++)
+		{
+			result.elements_[i] = new T[collumns_];
 		}
 		result.rows_ = rows_;
 		result.collumns_ = collumns_;
-		
-		for (std::size_t i = 0; i<rows_; i++) {
-			for (std::size_t j = 0; j<collumns_; j++) {
+
+		for (std::size_t i = 0; i < rows_; i++)
+		{
+			for (std::size_t j = 0; j < collumns_; j++)
+			{
 				result.elements_[i][j] = elements_[i][j] - other.elements_[i][j];
 			}
 		}
 	}
-	else {
-		throw false;
+	else
+	{
+		std::cout << "An error has occured while reading input data\n";
 	}
 
 	return result;
 }
 
 template <typename T>
-matrix_t<T> matrix_t<T>::operator *( matrix_t<T> const & other ) const
+matrix_t<T> matrix_t<T>::operator*(matrix_t<T> const& other) const
 {
 	matrix_t<T> result;
-	
-	if (collumns_ == other.rows_) {
-  		result.elements_ = new T *[rows_];
-		for (std::size_t i = 0; i<rows_; i++) {
-			result.elements_[i] = new T [other.collumns_];
-	    }
-	    result.rows_ = rows_;
-	    result.collumns_ = other.collumns_;
-	    
-		for (std::size_t i = 0; i < rows_; ++i) {
-			for (std::size_t j = 0; j < other.collumns_; ++j) {
-				int result_ = 0;
-				for (std::size_t k = 0; k < other.rows_; ++k) {
+
+	if (collumns_ == other.rows_)
+	{
+		result.elements_ = new T*[rows_];
+		for (std::size_t i = 0; i < rows_; i++)
+		{
+			result.elements_[i] = new T[other.collumns_];
+		}
+		result.rows_ = rows_;
+		result.collumns_ = other.collumns_;
+
+		for (std::size_t i = 0; i < rows_; ++i)
+		{
+			for (std::size_t j = 0; j < other.collumns_; ++j)
+			{
+				std::size_t result_ = 0;
+				for (std::size_t k = 0; k < other.rows_; ++k)
+				{
 					result_ += elements_[i][k] * other.elements_[k][j];
 				}
 				result.elements_[i][j] = result_;
 			}
 		}
 	}
-	else {
-		throw false;
+	else
+	{
+		std::cout << "An error has occured while reading input data\n";
 	}
-
 	return result;
 }
 
 template <typename T>
-matrix_t<T> & matrix_t<T>::operator -=( matrix_t<T> const & other )
+matrix_t<T>& matrix_t<T>::operator-=(matrix_t<T> const& other)
 {
-	if (rows_ == other.rows_ && collumns_ == other.collumns_) {
-		for (std::size_t i = 0; i<rows_; i++) {
-			for (std::size_t j = 0; j<collumns_; j++) {
+	if (rows_ == other.rows_ && collumns_ == other.collumns_)
+	{
+		for (std::size_t i = 0; i < rows_; i++)
+		{
+			for (std::size_t j = 0; j < collumns_; j++)
+			{
 				elements_[i][j] -= other.elements_[i][j];
 			}
 		}
 	}
-	else {
-		throw false;
+	else
+	{
+		std::cout << "An error has occured while reading input data\n";
 	}
-	
+
 	return *this;
 }
 
 template <typename T>
-matrix_t<T> & matrix_t<T>::operator +=( matrix_t<T> const & other )
+matrix_t& matrix_t<T>::operator+=(matrix_t<T>const& other)
 {
-	if (rows_ == other.rows_ && collumns_ == other.collumns_) {
-		for (std::size_t i = 0; i<rows_; i++) {
-			for (std::size_t j = 0; j<collumns_; j++) {
+	if (rows_ == other.rows_ && collumns_ == other.collumns_)
+	{
+		for (std::size_t i = 0; i < rows_; i++)
+		{
+			for (std::size_t j = 0; j < collumns_; j++)
+			{
 				elements_[i][j] += other.elements_[i][j];
 			}
 		}
 	}
-	else {
-		throw false;
+	else
+	{
+		std::cout << "An error has occured while reading input data\n";
 	}
-	
+
 	return *this;
 }
 
 template <typename T>
-matrix_t<T> & matrix_t<T>::operator *=( matrix_t<T> const & other )
+matrix_t<T>& matrix_t<T>::operator*=(matrix_t<T> const& other)
 {
 	matrix_t<T> result;
-	
-	if (collumns_ == other.rows_) {
-		result.elements_ = new T *[rows_];
-		for (std::size_t i = 0; i<rows_; i++) {
-			result.elements_[i] = new T [other.collumns_];
-	    }
-	    result.rows_ = rows_;
-	    result.collumns_ = other.collumns_;
-	    
-		for (std::size_t i = 0; i < rows_; ++i) {
-			for (std::size_t j = 0; j < other.collumns_; ++j) {
-				int result_ = 0;
-				for (std::size_t k = 0; k < other.rows_; ++k) {
+
+	if (collumns_ == other.rows_)
+	{
+		result.elements_ = new T*[rows_];
+		for (std::size_t i = 0; i < rows_; i++)
+		{
+			result.elements_[i] = new T[other.collumns_];
+		}
+		result.rows_ = rows_;
+		result.collumns_ = other.collumns_;
+
+		for (std::size_t i = 0; i < rows_; ++i)
+		{
+			for (std::size_t j = 0; j < other.collumns_; ++j)
+			{
+				std::size_t result_ = 0;
+				for (std::size_t k = 0; k < other.rows_; ++k)
+				{
 					result_ += elements_[i][k] * other.elements_[k][j];
 				}
 				result.elements_[i][j] = result_;
@@ -199,115 +242,85 @@ matrix_t<T> & matrix_t<T>::operator *=( matrix_t<T> const & other )
 		}
 		*this = result;
 	}
-	else {
-		throw false;
+	else
+	{
+		std::cout << "An error has occured while reading input data\n";
 	}
-	
+
 	return *this;
 }
 
 template <typename T>
-bool matrix_t<T>::success (matrix_t<T> const & one, char op) {
-	matrix_t<T> result;
-	bool str = true;
-	
-	switch (op) {
-		case '+': {
-			try {
-				result = *this + one;
+std::istream& matrix_t<T>::read(std::istream& stream)
+{
+	std::size_t rows;
+	std::size_t collumns;
+	char symbol;
+
+	bool success = true;
+	if (stream >> rows && stream >> symbol && symbol == ',' && stream >> collumns)
+	{
+		float** elements = new T*[rows];
+		for (std::size_t i = 0; success && i < rows; ++i)
+		{
+			elements[i] = new T[collumns];
+			for (std::size_t j = 0; j < collumns; ++j)
+			{
+				if (!(stream >> elements[i][j]))
+				{
+					success = false;
+					break;
+				}
 			}
-			catch (bool s) {
-				str = s;
-			}
-			break;
 		}
-			
-		case '-': {
-			try {
-				result = *this -one;
+
+		if (success)
+		{
+			for (std::size_t i = 0; i < rows_; ++i)
+			{
+				delete[] elements_[i];
 			}
-			catch (bool s) {
-				str = s;
-			}
-			break;
+			delete[] elements_;
+
+			rows_ = rows;
+			collumns_ = collumns;
+			elements_ = elements;
 		}
-			
-		case '*': {
-			try {
-				result = (*this) * one;
+		else
+		{
+			for (std::size_t i = 0; i < rows; ++i)
+			{
+				delete[] elements[i];
 			}
-			catch (bool s) {
-				str = s;
-			}
-			break;
+			delete[] elements;
 		}
 	}
-	
-	return str;
-}
+	else
+	{
+		success = false;
+	}
 
-template <typename T>
-std::istream & matrix_t<T>::read( std::istream & stream )
-{
-    std::size_t rows;
-    std::size_t collumns;
-    char symbol;
-    
-    bool success = true;
-    if( stream >> rows && stream >> symbol && symbol == ',' && stream >> collumns ) {
-        float ** elements = new T *[ rows ];
-        for( std::size_t i = 0; success && i < rows; ++i ) {
-            elements[ i ] = new T[ collumns ];
-            for( std::size_t j = 0; j < collumns; ++j ) {
-                if( !( stream >> elements[ i ][ j ] ) ) {
-                    success = false;
-                    break;
-                }
-            }
-        }
-        
-        if( success ) {
-            for( std::size_t i = 0; i < rows_; ++i ) {
-                delete [] elements_[ i ];
-            }
-            delete [] elements_;
-            
-            rows_ = rows;
-            collumns_ = collumns;
-            elements_ = elements;
-        }
-        else {
-            for( std::size_t i = 0; i < rows; ++i ) {
-                delete [] elements[ i ];
-            }
-            delete [] elements;
-        }
-    }
-    else {
-        success = false;
-    }
-    
-    if( !success ) {
-        stream.setstate( std::ios_base::failbit );
-    }
-    
+	if (!success)
+	{
+		stream.setstate(std::ios_base::failbit);
+	}
+
 	return stream;
 }
 
 template <typename T>
-std::ostream & matrix_t<T>::write( std::ostream & stream ) const
+std::ostream & matrix_t<T>::write(std::ostream & stream) const
 {
-    stream << rows_ << ", " << collumns_;
-    for( std::size_t i = 0; i < rows_; ++i ) {
-	stream << '\n';
-        for( std::size_t j = 0; j < collumns_; ++j ) {
-            stream << elements_[ i ][ j ];
-            if( j != collumns_ - 1 ) {
-                stream << ' ';
-            }
-        }
-    }
-    if (collumns_ != rows_) stream << '\n';
-    
+	stream << rows_ << ", " << collumns_;
+	for (std::size_t i = 0; i < rows_; ++i) {
+		stream << '\n';
+		for (std::size_t j = 0; j < collumns_; ++j) {
+			stream << elements_[i][j];
+			if (j != collumns_ - 1) {
+				stream << ' ';
+			}
+		}
+	}
+
 	return stream;
 }
